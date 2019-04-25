f280840e217ab43f9600d587122a6b3f19071a65
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2019-04-25 22:08:30 +0700

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var howYourBody = document.getElementById("bmi"),
+    yourBMI = document.getElementById('yourbmi');
 
 function calBMI(){
     mass = document.getElementById('mass').value,
@@ -5,16 +7,43 @@ function calBMI(){
     BMI = mass / (height * height);
     console.log('mass la', mass);
     console.log('height la', height);
-    console.log('BMI la2', BMI)
+    console.log('BMI la2', BMI);
     if(BMI < 18.5){
-        document.getElementById("bmi").innerHTML = "<h1>Bạn thiếu cân</h1>";
-
+        howYourBody.innerHTML = "Bạn thiếu cân";
+        yourBMI.innerHTML = "BMI = "+ BMI;
     }else if(BMI <= 25){
-        document.getElementById("bmi").innerHTML = "<h1>Bạn có thân hình cân đối</h1>";
+        howYourBody.innerHTML = "Bạn có thân hình cân đối";
+        yourBMI.innerHTML = "BMI = "+ BMI;
     }else{
-        document.getElementById("bmi").innerHTML = "<h1>Bạn thừa cân</h1>";
-
+        howYourBody.innerHTML = "Bạn thừa cân";
+        yourBMI.innerHTML = "BMI = "+ BMI;
     };
 };
-
-
+var yourNumber = document.getElementById('your-number');
+function checkNumber(){
+    number = document.getElementById('check-number').value;
+    console.log('nuber is ', number);
+    calNumber = number % 3;
+    console.log("number cal", calNumber);
+    if ( calNumber == 0){
+        yourNumber.innerHTML = "số của bạn chia hết cho 3"
+    }else {
+        yourNumber.innerHTML = "số của bạn không chia hết cho 3"
+    }
+}
+var checkYear = document.getElementById('leap-year');
+function checkLeapYear(){
+    userYear = document.getElementById('check-leap-year').value;
+    console.log('years = '  ,userYear);
+    if(userYear % 100 == 0){
+        if(userYear % 400 ==0){
+            checkYear.innerHTML = "năm " + userYear + " là năm nhuận"; 
+        }else{
+            checkYear.innerHTML = "năm " + userYear + " không phải là năm nhuận"; 
+        }
+    }else if(userYear % 4 == 0){
+        checkYear.innerHTML = "năm " + userYear + " là năm nhuận"; 
+    }else{
+        checkYear.innerHTML = "năm " + userYear + " không phải là năm nhuận"; 
+    }
+}
